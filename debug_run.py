8a0049946f0f298dc3d028aa1d3da0795775f2fc
savio <30656522+saviosailas@users.noreply.github.com>
2024-04-20 20:02:16 +0530

    added debug py

---
+++ debug_run.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv(os.path.join(os.getcwd(), ".env"), verbose=True, override=True)
+
+from witamy_server import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-source .env
 python -m flask --app witamy_server create-database
-python -m flask --app=witamy_server run --debug
+# python -m flask --app=witamy_server run --debug
+python -m flask --app=debug_run run --debug
